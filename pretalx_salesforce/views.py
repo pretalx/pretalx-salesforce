a182e13a9dc26489b96b4153ee510edde19ea4fc
Tobias Kunze <r@rixx.de>
2024-11-14 22:43:10 +0100

    Fix None comparison

--- pretalx_salesforce/views.py
+++ pretalx_salesforce/views.py
@@ -22,19 +22,23 @@ class SalesforceSettingsView(PermissionRequired, FormView):
 
     @context
     def last_sync(self):
-        last_speaker_sync = SpeakerProfileSalesforceSync.objects.order_by(
-            "last_synced"
-        ).last()
-        last_submission_sync = SubmissionSalesforceSync.objects.order_by(
-            "last_synced"
-        ).last()
+        last_speaker_sync = getattr(
+            SpeakerProfileSalesforceSync.objects.order_by("last_synced").last(),
+            "last_synced",
+            None,
+        )
+        last_submission_sync = getattr(
+            SubmissionSalesforceSync.objects.order_by("last_synced").last(),
+            "last_synced",
+            None,
+        )
         if not last_speaker_sync and not last_submission_sync:
             return None
         if not last_speaker_sync:
-            return last_submission_sync.last_synced
+            return last_submission_sync
         if not last_submission_sync:
-            return last_speaker_sync.last_synced
-        return max(last_speaker_sync.last_synced, last_submission_sync.last_synced)
+            return last_speaker_sync
+        return max(last_speaker_sync, last_submission_sync)
 
     def post(self, request, *args, **kwargs):
         if "sync-now" not in request.POST:
